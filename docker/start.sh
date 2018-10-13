428b1f92a330fa1a1ebc62ec52940daea1b5fe8b
mango63 <petershallue@hotmail.com>
2018-10-13 15:02:11 +1100

    Create start.sh

---
+++ docker/start.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+# Start server advertiser
+HOSTNAME=$( grep -o -P '(?<=motd=).*' server.properties )
+[[ -z "$HOSTNAME" ]] && HOSTNAME="A minecraft server"
+
+python3 advertise.py "$HOSTNAME" &
+
+# Start server itself
+
+exec java -Xms4G -Xmx4G -jar craftbukkit-1.13.1.jar nogui
