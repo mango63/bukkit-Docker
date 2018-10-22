1bd1aa9b45debae74bfcfdd5b6a596c41311f359
mango63 <petershallue@hotmail.com>
2018-10-23 07:19:39 +1100

    better build script

--- bukkit/build.sh
+++ bukkit/build.sh
@@ -2,9 +2,18 @@
 
 . ../common.sh
 
-[[ -z $VERSION ]] && VERSION=1.13.1
+[[ -z $VERSION ]] && VERSION=1.13.2
 
-[[ -f craftbukkit-$VERSION.jar ]]  || wget https://cdn.getbukkit.org/craftbukkit/craftbukkit-$VERSION.jar
+build(){
+     git config --global --unset core.autocrlf
+     java -jar BuildTools.jar --rev $VERSION
+}
 
+
+
+#[[ -f craftbukkit-$VERSION.jar ]]  || wget https://cdn.getbukkit.org/craftbukkit/craftbukkit-$VERSION.jar
+
+[[ -f BuildTools.jar ]]  || wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+[[ ! `ls craftbukkit*.jar` ]] && build
 
 docker_build bukkit
