8e13e03905d3c0c426897d19ed7c477bbb436791
Doug Ferris <priorax@gmail.com>
2018-10-23 21:07:51 +1100

    Added support for multistage docker build

--- bukkit/build.sh
+++ bukkit/build.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-
+SECONDS=0
 . ../common.sh
 
 [[ -z $VERSION ]] && VERSION=1.13.2
 
-build(){
-     git config --global --unset core.autocrlf
-     java -jar BuildTools.jar --rev $VERSION
-}
-
-
-
-#[[ -f craftbukkit-$VERSION.jar ]]  || wget https://cdn.getbukkit.org/craftbukkit/craftbukkit-$VERSION.jar
-
-[[ -f BuildTools.jar ]]  || wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-[[ ! `ls craftbukkit*.jar` ]] && build
-
 docker_build bukkit
+echo "Built in ${SECONDS} seconds"
